e8c06f2de81156673a24c08fddc0de3eb520c66f
Frank Tsai <franks0059@gmail.com>
2020-12-07 17:15:41 +0800

    First commit

--- server.js
+++ server.js
@@ -4,24 +4,41 @@ const compress = require('koa-compress')();
 const cors = require('@koa/cors')(/* Add your cors option */);
 const helmet = require('koa-helmet')(/* Add your security option */);
 const logger = require('koa-logger')();
+const { ApolloServer, gql } = require('apollo-server-koa');
 
 const errorHandler = require('./middleware/error.middleware');
 const applyApiMiddleware = require('./api');
 const { isDevelopment } = require('./config');
 
-const server = new Koa();
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = new Koa();
 
 /**
  * Add here only development middlewares
  */
 if (isDevelopment) {
-  server.use(logger);
+  app.use(logger);
 }
 
 /**
  * Pass to our server instance middlewares
  */
-server
+app
   .use(errorHandler)
   .use(helmet)
   .use(compress)
@@ -31,6 +48,8 @@ server
 /**
  * Apply to our server the api router
  */
-applyApiMiddleware(server);
+applyApiMiddleware(app);
 
-module.exports = server;
+server.applyMiddleware({ app });
+
+module.exports = app;
